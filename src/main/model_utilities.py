fd03875d2b6462bb71fc616cb5d8c2cc78356953
DacianGoina <dacian.goina00@e-uvt.ro>
2023-12-30 19:18:25 +0200

    create-prediction-flow2

--- src/main/model_utilities.py
+++ src/main/model_utilities.py
@@ -33,13 +33,13 @@ def get_model_evaluation_metrics(confusion_matrix):
     accuracy = np.sum(true_positives) / total_instances
     precisions = true_positives / (true_positives + false_positives)
     recalls = true_positives / (true_positives + false_negatives)
-    specificity = true_negatives / (true_negatives + false_positives)
+    specificities = true_negatives / (true_negatives + false_positives)
     f1_scores = 2 * (precisions * recalls) / (precisions + recalls)
 
     metric_dict['accuracy'] = accuracy
     metric_dict['precision'] = round(np.sum(precisions)/len(precisions), 5)
     metric_dict['recall'] = round(np.sum(recalls)/len(recalls), 5)
-    metric_dict['specificity'] = round(np.sum(specificitys)/len(specificity), 5)
+    metric_dict['specificity'] = round(np.sum(specificitys)/len(specificities), 5)
     metric_dict['f1_score'] = round(np.sum(f1_scores)/len(f1_scores), 5)
 
     return metric_dict
