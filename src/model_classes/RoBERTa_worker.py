172ce835b13bd5b434beff80304f8a8bd5e88b07
DacianGoina <dacian.goina00@e-uvt.ro>
2024-01-13 19:26:08 +0200

    added roberta

--- src/model_classes/RoBERTa_worker.py
+++ src/model_classes/RoBERTa_worker.py
@@ -7,8 +7,10 @@ def load_transformer():
 
     return roberta_dcm
 
-
-def get_type_substitutions():
+# IN: boolean reverse
+# OUT: dict with labels to integers values
+# if reverse is True, the data and keys are flipped
+def get_type_substitutions(reverse = False):
     type_substitution = {
         'business':0,
         'entertainment':1,
@@ -22,21 +24,37 @@ def get_type_substitutions():
         'technology':9
     }
 
+    rev_type_substitution = {data_value:key_value for key_value, data_value in type_substitution.items()}
+
+    if reverse is True:
+        return rev_type_substitution
+
     return type_substitution
 
-def predict(file_path):
+
+# predict but use content direct from a txt file
+def predict_from_file(file_path):
+    input_content = read_txt_file(file_path)
+
+    return predict(input_content)
+
+
+# perform prediction for a single text
+# IN: str raw text
+# OUT: predicted label
+def predict(raw_text):
+    raw_text = [raw_text]
 
     roberta_dcm = load_transformer()
+    substitutions_dict = get_type_substitutions(reverse=True)
+    predicted_label, raw_output = roberta_dcm.predict(raw_text)
+    predicted_label = int(predicted_label[0])
 
-    input_content = read_txt_file(file_path)
-    input_content = [input_content]
-    predicted_label, raw_output = roberta_dcm.predict(input_content)
+    return substitutions_dict[predicted_label]
 
-    print(type(roberta_dcm))
-    print(predicted_label)
-
-    return predicted_label
 
 if __name__ == "__main__":
     print("RoBERTa Worker")
-    predict("../../testing_files/history_doc.txt")
+    #pred = predict_from_file("../../testing_files/history_doc.txt")
+    pred = predict("Usain Bolt is a great runner")
+    print(pred)
